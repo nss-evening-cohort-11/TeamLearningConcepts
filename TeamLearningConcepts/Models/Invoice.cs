5662d980252a526b59d697723d602ea9b59bb7e9
Kayla Melton <kannmelton2@gmail.com>
2020-10-21 18:56:49 -0700

    wip created invoice model class

---
+++ TeamLearningConcepts/Models/Invoice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamLearningConcepts.Models
+{
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+        public int PaymentTypeId { get; set; }
+        public int UserId { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public decimal InvoiceTotal { get; set; }
+        public bool isCompleted { get; set; } 
+    }
+}
