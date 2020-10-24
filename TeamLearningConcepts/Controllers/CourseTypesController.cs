c3f5c5e9002e9b6f380e90168cc613198fb5b8f1
Joey Petrone <joey.petrone@gmail.com>
2020-10-24 11:20:53 -0500

    Course type data complete

---
+++ TeamLearningConcepts/Controllers/CourseTypesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamLearningConcepts.Data;
+using TeamLearningConcepts.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeamLearningConcepts.Controllers
+{
+    [Route("api/courseTypes")]
+    [ApiController]
+    public class CourseTypesController : ControllerBase
+    {
+        CourseTypeRepository _repo;
+
+        public CourseTypesController()
+        {
+            _repo = new CourseTypeRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllCourseTypes()
+        {
+            var allCourseTypes = _repo.GetAll();
+
+            return Ok(allCourseTypes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingleCourseType(int id)
+        {
+            var singleCourseType = _repo.GetCourseTypeById(id);
+
+            return Ok(singleCourseType);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCourseType(int id)
+        {
+            if (_repo.GetCourseTypeById(id) == null)
+            {
+                return NotFound();
+            }
+
+            _repo.Remove(id);
+
+            return Ok();
+        }
+
+    }
+}
