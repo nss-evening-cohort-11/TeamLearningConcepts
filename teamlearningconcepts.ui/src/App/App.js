59296242a8ed98942794e40612a35558fcda3713
Todd Spainhour <toddspainhour@gmail.com>
2020-10-28 19:17:21 -0500

    LatestCourses component is displaying in App.js

--- teamlearningconcepts.ui/src/App/App.js
+++ teamlearningconcepts.ui/src/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
 import Users from '../components/pages/Users/Users';
+import LatestCourses from '../components/shared/LatestCourses/LatestCourses';
 
 function App() {
   return (
     <div className="App">
+      <LatestCourses />
       <Users />
     </div>
   );
---
+++ teamlearningconcepts.ui/src/components/shared/LatestCourses/LatestCourses.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class LatestCourses extends React.Component {
+  render() {
+
+    return(
+      <div>
+        <h5>Inside the LatestCourses component.</h5>
+      </div>
+    )
+  }
+}
+
+export default LatestCourses;
