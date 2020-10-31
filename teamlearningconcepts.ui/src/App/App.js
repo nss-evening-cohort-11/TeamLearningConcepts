49dc85098817ffb3ab56ec5c024a8dcd9208e358
Todd Spainhour <toddspainhour@gmail.com>
2020-10-31 12:19:07 -0500

    LatestCourses component displaying in App.js

--- teamlearningconcepts.ui/src/App/App.js
+++ teamlearningconcepts.ui/src/App/App.js
@@ -3,11 +3,13 @@ import './App.scss';
 import Courses from '../components/pages/Courses/Courses';
 import MyNavbar from '../components/shared/MyNavbar/MyNavbar';
 import Users from '../components/pages/Users/Users';
+import LatestCourses from '../components/shared/LatestCourses/LatestCourses';
 
 function App() {
   return (
     <div className="App">
       <MyNavbar />
+      <LatestCourses />
       <Courses />
       <Users />
     </div>
---
+++ teamlearningconcepts.ui/src/components/shared/LatestCourses/LatestCourses.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class LatestCourses extends React.Component {
+  render() {
+    return(
+      <h3>This is your Latest Courses Component</h3>
+    )
+  }
+}
+
+export default LatestCourses;
+
