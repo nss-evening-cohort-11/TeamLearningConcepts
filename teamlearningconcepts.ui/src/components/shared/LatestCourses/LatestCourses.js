11d083b990bdcf3a7465351bc87a90db95d64e9b
Todd Spainhour <toddspainhour@gmail.com>
2020-10-31 18:49:34 -0500

    LatestCourses component displaying last 5 courses from database

--- teamlearningconcepts.ui/src/components/shared/LatestCourses/LatestCourses.js
+++ teamlearningconcepts.ui/src/components/shared/LatestCourses/LatestCourses.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import couseData from '../../../helpers/data/courseData';
+import SingleCourseCard from '../SingleCourseCard/SingleCourseCard';
 
 
 class LatestCourses extends React.Component {
+
+  state = { latestCourses: [] }
+
+  componentDidMount() {
+    console.log("inside your componentDidMount in LatestCourse.js")
+    couseData.getLatestCourses()
+    .then(latestCourses => { this.setState({latestCourses})});
+  }
+
+
   render() {
+
+    const {latestCourses} = this.state;
+    console.log(typeof latestCourses);
+    const buildLatestCoursesCards = latestCourses.map((course) => {
+      return (<SingleCourseCard course={course} />)
+    })
+
+
     return(
+      <div>
       <h3>This is your Latest Courses Component</h3>
+      {buildLatestCoursesCards}
+      </div>
     )
   }
 }
