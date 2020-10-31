f0d33f86fc9e52157447fc44ddbd353f1b35468a
Joey Petrone <joey.petrone@gmail.com>
2020-10-31 12:10:14 -0500

    Single Category Component created

---
+++ teamlearningconcepts.ui/src/components/pages/SingleCategory/SingleCategory.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SingleCategory.scss';
+
+class SingleCategory extends React.Component {
+  render() {
+    return (
+      <div className="SingleCategory">
+        In SingleCategory
+      </div>
+    )
+  }
+};
+
+export default SingleCategory;
