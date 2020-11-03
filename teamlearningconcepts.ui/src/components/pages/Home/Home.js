76cbccd123b9cf84767b0977ebe5544b62d2ce93
Michele Rawlins <shellbop@att.net>
2020-11-02 21:03:34 -0600

    home page mvp

--- teamlearningconcepts.ui/src/components/pages/Home/Home.js
+++ teamlearningconcepts.ui/src/components/pages/Home/Home.js
@@ -1,20 +1,33 @@
 import React from 'react';
 
-// import authData from '../../../helpers/data/authData';
-
+import Example from '../../shared/Example/Example';
 
 import './Home.scss';
 
 class Home extends React.Component {
+  state = {
+    Example: [],
+  }
+
+
+
+  render() {
   
-    render() {
-    
     return (
       <div className="Home container">
-        <p>This is home</p>
-         </div>
-          );
-        }
-      }
-      
+      <h2 className ="title">Welcome to Team Learning Concepts</h2>
+      <h5 className ="tagLine">Are you a business professional looking to improve your skillset?</h5>
+      <img className ="introPic"src="https://live.staticflickr.com/65535/50559997591_d589d5a648_n.jpg" width="320" height="213" alt="laptopPic"></img>
+      <img className ="introPic" src="https://live.staticflickr.com/65535/50559998796_365ce1df31_n.jpg" width="320" height="213" alt="groupLearn"></img>
+      <h2 className = "featured">Featured Courses</h2>
+      <Example />
+   <h2 className = "salesCourses">Check out all our Sales Courses</h2>
+   <img className ="salesPic" src="https://images.unsplash.com/photo-1520694478166-daaaaec95b69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"  width="320" height="213" text="How to Retain Customers - $400"></img>
+  <img className = "salesPic" src="https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"  width="320" height="213" text="How to Navigate a Purchase Request - $350"></img>
+  <button>Shop Now</button><button>Shop Now</button>
+</div>
+         );
+    }
+    
+}  
       export default Home;
---
+++ teamlearningconcepts.ui/src/components/shared/Example/Example.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
+
+const items = [
+    {
+      src: 'https://images.unsplash.com/photo-1520694478166-daaaaec95b69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+      altText: 'Slide 1',
+      caption: 'How to Retain Customers - $400.00'
+    },
+    {
+      src: 'https://cdn.pixabay.com/photo/2015/11/06/12/35/control-1027103_1280.jpg',
+      altText: 'Slide 2',
+      caption: 'Taxes for Solo Enterpreneur - $800.00'
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1573167507387-6b4b98cb7c13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+      altText: 'Slide 3',
+      caption: 'How to Run an effective Meeting - $1,000.00'
+    }
+];
+
+const Example = (props) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  }
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img src={item.src} alt={item.altText} />
+        <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <Carousel
+      activeIndex={activeIndex}
+      next={next}
+      previous={previous}
+    >
+      <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+      {slides}
+      <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+      <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+    </Carousel>
+  );
+}
+
+export default Example;
