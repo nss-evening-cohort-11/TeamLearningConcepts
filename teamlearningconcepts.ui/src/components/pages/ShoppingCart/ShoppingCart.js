526b1f98a9e3aaf8852a13c5b4dddffc5d9d22de
Kayla Melton <kannmelton2@gmail.com>
2020-11-04 17:11:04 -0800

    added to and styled the shopping cart page

--- teamlearningconcepts.ui/src/components/pages/ShoppingCart/ShoppingCart.js
+++ teamlearningconcepts.ui/src/components/pages/ShoppingCart/ShoppingCart.js
@@ -6,7 +6,29 @@ class ShoppingCart extends React.Component {
     render(){
         return(
             <div className="ShoppingCart">
-                <h1>Shopping Cart</h1>
+                <div className="shopping-cart-progress d-flex">
+                    <p className="shopping-cart-border">1. Shopping Cart</p>
+                    <p className="payment-options-border">2. Payment Options</p>
+
+                </div>
+                <div className="shopping-cart-summary-area d-flex">
+                <div className="w-75 shopping-cart-items">
+                <p className="cart-title">Shopping Cart</p>
+                <hr />
+                <p>Course cards here</p>
+                <hr />
+                <button className="btn w-50 btn-light">Next</button>
+                </div>
+                <div className="w-25 shopping-summary">
+                <p className="cart-title">Summary</p>
+                <hr />
+                <p>Subtotal:</p>
+                <p>Taxes:</p>
+                <hr />
+                <p>Total:</p>
+                </div>
+                </div>
+
             </div>
         )
     }
