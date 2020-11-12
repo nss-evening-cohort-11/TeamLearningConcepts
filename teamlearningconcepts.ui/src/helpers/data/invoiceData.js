777601a98ccfdcb7e9841f9dc61c0e3260f9807f
Joey Petrone <joey.petrone@gmail.com>
2020-11-11 18:11:54 -0600

    wip

--- teamlearningconcepts.ui/src/helpers/data/invoiceData.js
+++ teamlearningconcepts.ui/src/helpers/data/invoiceData.js
@@ -3,13 +3,13 @@ import axios from 'axios';
 import {baseUrl} from './constants.json';
 
 const putCompletedInvoice = (invoiceId, paymentTypeId, invoiceTotal) => 
-    axios.put(
-      `${baseUrl}/invoices/complete`, 
-      {
-        InvoiceId: invoiceId,
-        PaymentTypeId: paymentTypeId,
-        InvoiceTotal: invoiceTotal
-      }
-      );
+    axios.put(`${baseUrl}/invoices/complete`, {
+      InvoiceId: 1,
+      PaymentTypeId: 1,
+      UserId: 1,
+      // InvoiceDate: '2020-10-20 18:51:03.540',
+      // InvoiceTotal: 1000.00,
+      // IsCompleted: 1
+      });
 
 export default {putCompletedInvoice};
