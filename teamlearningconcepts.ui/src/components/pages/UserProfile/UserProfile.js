f00d41092ffeece673215b746b3a1fec94021af7
Todd Spainhour <toddspainhour@gmail.com>
2020-11-16 19:36:29 -0600

    UserProfile component created

---
+++ teamlearningconcepts.ui/src/components/pages/UserProfile/UserProfile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './UserProfile.scss';
+
+
+class UserProfile extends React.Component {
+render() {
+  return(
+    <div className="UserProfile">
+      <p>This is the UserProfile component</p>
+    </div>
+  )
+}
+}
+
+export default UserProfile 
