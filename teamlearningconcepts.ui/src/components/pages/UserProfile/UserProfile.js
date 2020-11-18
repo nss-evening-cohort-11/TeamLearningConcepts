567e0b20132f24b73b73479c9637037dbb4fa30c
Todd Spainhour <toddspainhour@gmail.com>
2020-11-17 20:02:06 -0600

    added axios call to return a hardcoded user's profile info and put in state

--- teamlearningconcepts.ui/src/components/pages/UserProfile/UserProfile.js
+++ teamlearningconcepts.ui/src/components/pages/UserProfile/UserProfile.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import userData from '../../../helpers/data/userData';
 import './UserProfile.scss';
 
 
 class UserProfile extends React.Component {
+
+  state = {
+    userProfile: []
+  }
+
+  componentDidMount() {
+    userData.getSingleUser()
+    .then(userProfile => { this.setState({userProfile}) })
+  }
+
 render() {
+  // const {users} = this.state;
+  // const buildUserProfile = userProfile.map((user) => {
+    // return user
+  // })
   return(
     <div className="UserProfile">
-      <p>This is the UserProfile component</p>
+      <h1>This is the UserProfile component</h1>
     </div>
   )
 }
