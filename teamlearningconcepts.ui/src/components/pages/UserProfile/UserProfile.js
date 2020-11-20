3f2cadab4365c7603882def81e9be0c00c90a381
Todd Spainhour <toddspainhour@gmail.com>
2020-11-19 21:01:53 -0600

    displaying all relevant user info

--- teamlearningconcepts.ui/src/components/pages/UserProfile/UserProfile.js
+++ teamlearningconcepts.ui/src/components/pages/UserProfile/UserProfile.js
@@ -39,12 +39,26 @@ render() {
   
 const {userProfile} = this.state;
   return(
-    <div className="UserProfile">
-  <h2>{`${userProfile.firstName} ${userProfile.lastName}'s Profile`}</h2>
-  <img src={userProfile.photoUrl} alt={`"${userProfile.firstName}'s Profile Pic"`}></img>
-  <h6>My auto generated SQL userId is: {userProfile.userId}</h6>
+    <div className="UserProfile col-md-12">
+      <h2>{`${userProfile.firstName} ${userProfile.lastName}'s Profile`}</h2>
 
-  <button className="btn btn-warning">Previous Orders</button>
+        <div className="profile-container">
+          <img src={userProfile.photoUrl} className="userProfileImage col-4"alt={`"${userProfile.firstName}'s Profile Pic"`}></img>
+          <div className="profile-info col-12">
+          <p className="profile-line-item"><span className="line-title">Profile Name:</span>{userProfile.username}</p>
+          <p className="profile-line-item"><span className="line-title">First Name:</span> {userProfile.firstName}</p>
+          <p className="profile-line-item"><span className="line-title">Last Name:</span> {userProfile.lastName}</p>
+          <p className="profile-line-item"><span className="line-title">Email Address:</span> {userProfile.email}</p>
+          <p className="profile-line-item"><span className="line-title">My auto generated SQL userId is:</span> {userProfile.userId}</p>
+        </div>
+
+        </div>
+
+      
+
+      <div>
+      <button className="btn btn-warning">Previous Orders</button>
+      </div>
     </div>
   )
 }
