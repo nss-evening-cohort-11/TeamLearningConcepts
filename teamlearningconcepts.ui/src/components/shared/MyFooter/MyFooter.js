f401af9700ea3a3bff3c430f04ff8a9a1e05fed4
Kayla Melton <kannmelton2@gmail.com>
2020-11-20 18:37:15 -0800

    wip footer styling

--- teamlearningconcepts.ui/src/components/shared/MyFooter/MyFooter.js
+++ teamlearningconcepts.ui/src/components/shared/MyFooter/MyFooter.js
@@ -5,7 +5,7 @@ import './MyFooter.scss';
 class MyFooter extends React.Component {
     render() {
         return(
-            <div className="MyFooter">
+            <div className="MyFooter d-flex flex-column min-vh-100">
                 <p className="text-center tlc-title">Team Learning Concepts | Copyright 2020</p>
             </div>
         )
